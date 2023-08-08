840777b2b155d8cb95f38e81cfcc9d5f823eabc0
KAUAN ALLIEN ALVES DA SILVA <kauan.4906@aluno.pr.senac.br>
2023-08-08 14:58:31 -0300

    Criação do Jogo Breakout

--- Currículo/Atari Game ( Quebra Tijolo)/scriPT.js
+++ Currículo/Atari Game ( Quebra Tijolo)/scriPT.js
@@ -4,7 +4,7 @@ var desenho = canvas.getContext("2d");
 
 //Configurar à Raquete//
 var raqueteAltura = 10;                      //Altura da Raquete
-var raqueteLargura = 75;                    //Largura da Raquete
+var raqueteLargura = 70;                    //Largura da Raquete
 var raqueteX = (canvas.width - raqueteLargura) / 2; //centralizar raquete
 var velocidadeRaquete = 15;
 
@@ -12,8 +12,8 @@ var velocidadeRaquete = 15;
 var bolaRadius = 10;
 var bolaX = canvas.width / 2;
 var bolaY = canvas.height - 30;                      
-var bolaDX = 4;                                            //direção da bola em X (esquerda/direita)
-var bolaDY = -4;                                           //direção da bola em Y (acima/baixo)
+var bolaDX = 1;                                            //direção da bola em X (esquerda/direita)
+var bolaDY = -1;                                           //direção da bola em Y (acima/baixo)
 
 var tijolosPorLinha = 5;
 var tijolosPorColuna = 8;
@@ -27,17 +27,83 @@ var Tijolos = [];              //Lista de Tijolos
 var totalPontuação = tijolosPorLinha * tijolosPorColuna * 10;
 var pontuacao = 0;
 
-//Dedicado apenas a inicialização dos tijolos
-for( var coluna=0; coluna < tijolosPorColuna; coluna++  ){
-    Tijolos[coluna] = []   //012345
-
-    for(var linha=0; linha < tijolosPorLinha; linha++ ){
-        Tijolos[coluna][linha] = {x:0, y:0, ativo:1 }
-        //X é a posição esquerda/direita no canva
-        //Y é a posição acima/abaixo no canva
-        //Ativo, determina se o tijolo aparece ou não, 1 ou 0
-    }
+function Easy(){
+    raqueteLargura = 90;
+    tijolosPorLinha = 3;
+    tijolosPorColuna = 5;
+    TijoloAltura = 40;
+    TijoloLargura = 90;
+    bolaRadius = 15;
+    bolaDX = 2;
+    bolaDY = -1;
+    totalPontuação = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30;
+    iniciarTijolos();
 }
+
+
+function Normal(){
+    raqueteLargura =80 ;
+    tijolosPorLinha = 5;
+    tijolosPorColuna = 7;
+    TijoloAltura = 20;
+    TijoloLargura = 65;
+    bolaRadius = 10 ;
+    bolaDX = 7;
+    bolaDY = -5 ;
+    totalPontuação = tijolosPorLinha * tijolosPorColuna * 10;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30;
+    iniciarTijolos();
+}
+
+function Hard(){
+    raqueteLargura = 70 ;
+    tijolosPorLinha = 7 ;
+    tijolosPorColuna = 7;
+    TijoloAltura = 20 ;
+    TijoloLargura = 65 ;
+    bolaRadius = 10;
+    bolaDX = 8;
+    bolaDY = -8 ;
+    totalPontuação = tijolosPorLinha * tijolosPorColuna * 10;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30;
+    iniciarTijolos();
+}
+
+function Master(){
+    raqueteLargura = 60 ;
+    tijolosPorLinha = 8 ;
+    tijolosPorColuna = 7 ;
+    TijoloAltura = 20 ;
+    TijoloLargura = 65 ;
+    bolaRadius = 10 ;
+    bolaDX = 9 ;
+    bolaDY = -10 ;
+    totalPontuação = tijolosPorLinha * tijolosPorColuna * 10;
+    bolaX = canvas.width / 2;
+    bolaY = canvas.height - 30 ;
+    iniciarTijolos();
+}
+
+
+function iniciarTijolos(){
+    //Dedicado apenas a inicialização dos tijolos
+    for( var coluna=0; coluna < tijolosPorColuna; coluna++  ){
+        Tijolos[coluna] = []   //012345
+
+        for(var linha=0; linha < tijolosPorLinha; linha++ ){
+            Tijolos[coluna][linha] = {x:0, y:0, ativo:1 }
+            //X é a posição esquerda/direita no canva
+            //Y é a posição acima/abaixo no canva
+            //Ativo, determina se o tijolo aparece ou não, 1 ou 0
+        }
+    }    
+}            
+iniciarTijolos();
 
 
 
@@ -80,7 +146,6 @@ function desenharRaquete(){
     desenho.fill();
     desenho.closePath();
     
-
 }
 
 function desenharBola(){
@@ -130,9 +195,10 @@ function detectarColisão(){
                         tela = document.getElementById("Screen");
                         pontuacao = pontuacao + 10;
                         tela.innerHTML = "Score: " + pontuacao;
+                        gerarEfeitoSonoro('moeda.mp3');
 
                         if(pontuacao === totalPontuação){
-                            window.location.reload();
+                            YouWin();
                         }
                         
                 }
@@ -144,11 +210,45 @@ function detectarColisão(){
 function GameOver(){
     var GameOver = document.getElementById("GameOver");
     GameOver.style.display = "block";
+    bolaDX = 0;
+    bolaDY = 0;
+    gerarEfeitoSonoro('GameOver.mp3');    
 }
 
 function Reiniciar(){
     document.location.reload();
 }
+
+//Aparecer na tela mensagem de Vitoria 
+function YouWin(){
+    var mensagem = document.getElementById("YouWin");
+    mensagem.style.display = "block";
+    bolaDX = 0;
+    bolaDY = 0;
+    gerarEfeitoSonoro('YouWin(SF2).mp3');
+}
+
+function gerarEfeitoSonoro(som){
+    var contexto = new (window.AudioContext)();
+    //Fazer uma requisição para carregar um arquivo de som
+    var requisição = new XMLHttpRequest();
+    requisição.open('GET',som,true);
+    requisição.responseType = 'arraybuffer';    //armazenar Memoria
+
+    requisição.onload = function(){
+        //decodificar o arquivo de som
+        contexto.decodeAudioData(requisição.response, function(buffer){ 
+            //Reprodução de Som
+            var fonte = contexto.createBufferSource();
+            fonte.buffer = buffer;
+            // Conectar a saída de Som
+            fonte.connect(contexto.destination);
+            fonte.start(0);      //Executa o Som
+        });
+    }
+    requisição.send();
+}
+
 
 function desenhar(){
     desenho.clearRect(0 ,0 , canvas.width, canvas.height );           //limpar o frame anterior
@@ -161,11 +261,13 @@ function desenhar(){
         //Analisar colisão de eixo X, colisão do canto direito/esquerdo
     if(bolaX + bolaDX > canvas.width - bolaRadius || bolaX + bolaDX < bolaRadius){
         bolaDX = - bolaDX; //Inverte direção direita/esquerda
+        gerarEfeitoSonoro('AKUAKU.mp3');
     }
 
     //Analisa colisão com parte de cima
     if(bolaY + bolaDY < bolaRadius){
         bolaDY = -bolaDY; //Inverte colisão ao bater em cima
+        gerarEfeitoSonoro('AKUAKU.mp3');
 
     }else if(bolaY + bolaDY > canvas.height - bolaRadius - raqueteAltura ){
 
@@ -173,6 +275,7 @@ function desenhar(){
         if(bolaX > raqueteX && bolaX < raqueteX + raqueteLargura){
 
             bolaDY = -bolaDY;                                    //Inverte Direção
+            gerarEfeitoSonoro('Mouse.mp3');
 
         }else{
             GameOver();
@@ -196,4 +299,3 @@ function desenhar(){
     requestAnimationFrame(desenhar)                      //Atualizar à tela de forma suave
 }
 desenhar();                                           //Chama função desenhar
-
